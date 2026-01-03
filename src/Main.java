3fc337712b396176c67e1fbca1054a80bd7e2fb0
문창현 <jake123456@naver.com>
2026-01-03 13:38:07 +0900

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,31 @@
 
+import test.BdecoderTest;
 import test.BencoderTest;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        testBencoderTest();
-
+        //testBencoderTest();
+        //testBdecodeTest();
     }
 
     public static void testBencoderTest(){
         BencoderTest bencoderTest = new BencoderTest();
 
         String announce = " http://tracker.example.com";
-        String filePath = "encodeFile.txt";
+        String filePath = "testFile.txt";
+        String encodedFilePath = "encoded";
 
-        bencoderTest.runBencoderTest(announce, filePath);
+        bencoderTest.runBencoderTest(announce, filePath, encodedFilePath);
     }
 
+    public static void testBdecodeTest(){
+        BdecoderTest bdecoderTest = new BdecoderTest();
+
+        String torrentFilePath = "encoded.torrent";
+        String decodedFilePath = "decoded.txt";
+
+        bdecoderTest.runBdecoderTest(torrentFilePath, decodedFilePath);
+    }
 }
